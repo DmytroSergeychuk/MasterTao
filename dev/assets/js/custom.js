dfbe4c67652545a7f73cad23fc0c3c4e91af89bf
Dmitriy Sergeychuk <sergeychuk.d20104@gmail.com>
2024-05-12 18:47:28 +0300

    fancybox false

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,6 @@
+  $(document).ready(function(){
+    $('.single-item').slick();
+    Fancybox.bind('[data-fancybox="gallery"]', {
+        // Your custom options for a specific gallery
+      });
+  })
