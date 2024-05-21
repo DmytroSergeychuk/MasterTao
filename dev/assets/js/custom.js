9277689d99ea25a4b8dd0daa992d5f565619f171
Dmitriy Sergeychuk <sergeychuk.d20104@gmail.com>
2024-05-21 13:56:52 +0300

    header desktop and header mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,6 +1,18 @@
-  $(document).ready(function(){
-    $('.single-item').slick();
-    Fancybox.bind('[data-fancybox="gallery"]', {
-        // Your custom options for a specific gallery
-      });
-  })
+
+
+$(document).ready(function () {
+  $('.single-item').slick();
+  Fancybox.bind('[data-fancybox="gallery"]', {
+
+  });
+
+})
+
+const hamburger = document.querySelector("#hamburger");
+const mobileContainer = document.querySelector(".mobile-container");
+function showHamburgerMenu() {
+  mobileContainer.classList.toggle('show');
+  hamburger.classList.toggle('is-active')
+}
+  hamburger.addEventListener('click', showHamburgerMenu)
+
