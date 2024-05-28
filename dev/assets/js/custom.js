9d7120f8d921f298c88cd87c5ec92c5424b328aa
Dmitriy Sergeychuk <sergeychuk.d20104@gmail.com>
2024-05-28 17:26:29 +0300

    first-screen descktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
   $('.single-item').slick();
   Fancybox.bind('[data-fancybox="gallery"]', {
   });
-
 })
 
+// Hamburger
 const hamburger = document.querySelector("#hamburger");
 const mobileContainer = document.querySelector(".mobile-container");
 function showHamburgerMenu() {
@@ -14,4 +14,25 @@ function showHamburgerMenu() {
   hamburger.classList.toggle('is-active')
 }
   hamburger.addEventListener('click', showHamburgerMenu)
+
+  // Swiper
+  const swiper = new Swiper('.swiper-info', {
+    spaceBetween: 40,
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    //   disableOnInteraction: false,
+    // },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
 
