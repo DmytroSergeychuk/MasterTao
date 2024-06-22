1c3ba44cfe0fa1458d78b559f45fc7a419a1a900
Dmitriy Sergeychuk <sergeychuk.d20104@gmail.com>
2024-06-22 13:44:09 +0300

    Fix: sliders and logo

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -18,8 +18,8 @@ function showHamburgerMenu() {
 
 hamburger.addEventListener('click', showHamburgerMenu)
 
-// Swiper
-const swiper = new Swiper('.swiper-info', {
+// Swiper__first-screen
+const swiperInfo = new Swiper('.swiper-info', {
   spaceBetween: 12,
   loop: true,
   // autoplay: {
@@ -37,7 +37,7 @@ const swiper = new Swiper('.swiper-info', {
   },
 
   breakpoints: {
-    1024: {
+    1025: {
       spaceBetween: 40,
       navigation: {
         enabled: true,
@@ -55,10 +55,89 @@ const fileInput = document.querySelectorAll('.input-box_input');
 
 
 fileInput.forEach(item => {
-  item.addEventListener('change', function() {
+  item.addEventListener('change', function () {
     const fileName = item.closest('.input-box').querySelector('.file-name')
     fileName.innerHTML = item.files[0].name;
   })
 })
 
 fileLabel.forEach(item => item.closest('.form__row').previousElementSibling.classList.add('special_gap'))
+
+
+// Swiper__advantages
+const swiperAdvantages = new Swiper('.swiper-advantages', {
+  spaceBetween: 12,
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    enabled: true,
+  },
+
+  breakpoints: {
+    1025: {
+      initalSlide: 0,
+      loop: false,
+      slierPerGroup: 8,
+      spaceBetween: 0,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        enabled: false,
+      },
+    }
+  }
+});
+
+// Swiper__tamplate
+
+const swiperTemplate = new Swiper('.swiper-template', {
+  speed: 700,
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    enabled: false
+  },
+
+  breakpoints: {
+
+    390: {
+      slidesPerView: 1,
+    },
+
+    1025: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        wnabled: false,
+      }
+    },
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        wnabled: false,
+      }
+    }
+  },
+});
+
